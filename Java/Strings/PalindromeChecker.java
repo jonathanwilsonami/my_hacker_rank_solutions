c951db7ae958f34904cc97f5f291ece882a75bec
jonwilami323 <jonwilami@gmail.com>
2021-03-15 17:24:24 -0400

    Create PalindromeChecker.java

---
+++ Java/Strings/PalindromeChecker.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+      
+        char fromLeft;
+        char fromRight;
+        boolean palindrome = true;
+        for(int i = 0; i < A.length(); i++){
+            fromLeft = A.charAt(i);
+            fromRight = A.charAt(A.length() - i - 1);
+            if(fromLeft != fromRight){
+                palindrome = false;
+            }
+
+        }
+      
+        if(palindrome) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        
+    }
+}
+
+
+
